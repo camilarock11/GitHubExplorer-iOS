222faebd048b1833651f28260d896eef8112fd6f
Camila Roque <camilarock11@hotmail.com>
2026-08-28 09:41:34 -0300

    feat: add app entry point

---
+++ GitHubExplorer/App/GitHubExplorerApp.swift
@@ -0,0 +1,12 @@
+import SwiftUI
+
+@main
+struct GitHubExplorerApp: App {
+    private let container = DependencyContainer.live
+
+    var body: some Scene {
+        WindowGroup {
+            RootView(searchViewModel: container.makeSearchViewModel())
+        }
+    }
+}
