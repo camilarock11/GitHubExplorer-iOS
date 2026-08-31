6dedf6a786235630a0ae0aea88d914e8ccf6ff10
Camila Roque <camilarock11@hotmail.com>
2026-08-31 10:27:28 -0300

    feat: add repository domain models

--- GitHubExplorer/Domain/Models/GitHubUser.swift
+++ GitHubExplorer/Domain/Models/GitHubUser.swift
@@ -21,3 +21,24 @@ struct GitHubUserProfile: Equatable {
     let following: Int
     let publicRepositories: Int
 }
+
+struct GitHubRepository: Identifiable, Equatable {
+    let id: Int
+    let name: String
+    let description: String?
+    let htmlURL: URL?
+    let language: String?
+    let stargazersCount: Int
+    let forksCount: Int
+    let updatedAt: Date?
+}
+
+struct UserRepositoriesPage: Equatable {
+    let repositories: [GitHubRepository]
+    let page: Int
+    let perPage: Int
+
+    var hasNextPage: Bool {
+        repositories.count == perPage
+    }
+}
