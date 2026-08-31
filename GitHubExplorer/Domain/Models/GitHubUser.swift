26878056add08f97a0ea21707a4d7f29d7f9134a
Camila Roque <camilarock11@hotmail.com>
2026-08-31 10:16:54 -0300

    feat: add user profile domain model

--- GitHubExplorer/Domain/Models/GitHubUser.swift
+++ GitHubExplorer/Domain/Models/GitHubUser.swift
@@ -6,3 +6,18 @@ struct GitHubUser: Identifiable, Hashable {
     let avatarURL: URL?
     let profileURL: URL?
 }
+
+struct GitHubUserProfile: Equatable {
+    let id: Int
+    let login: String
+    let name: String?
+    let avatarURL: URL?
+    let profileURL: URL?
+    let bio: String?
+    let company: String?
+    let location: String?
+    let websiteURL: URL?
+    let followers: Int
+    let following: Int
+    let publicRepositories: Int
+}
